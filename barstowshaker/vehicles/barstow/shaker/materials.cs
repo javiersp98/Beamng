ff064055e1d217353bafbe5c924fc9d542b3dd97
javiersp98 <38034215+javiersp98@users.noreply.github.com>
2019-02-03 13:25:19 +0100

    Barstow Shaker Hood

---
+++ barstowshaker/vehicles/barstow/shaker/materials.cs
@@ -0,0 +1,11 @@
+singleton Material(barstow_shaker)
+{
+   mapTo = "barstow_shaker";
+   diffuseMap[0] = "barstow_shaker.png";
+   specularPower[0] = "15";
+   useAnisotropic[0] = "1";
+   castShadows = "1";
+   translucent = "0";
+   alphaTest = "0";
+   alphaRef = "0";
+};
